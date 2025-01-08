7038f9209a80386c0ff1e470378fb804702d4b6a
asklas <asklas+jyjy0372@gmail.com>
2025-01-08 17:33:33 +0900

    기본설정

---
+++ src/main/java/org/couplow/CouplowApplication.java
@@ -0,0 +1,13 @@
+package org.couplow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouplowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouplowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/couplow/CouplowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.couplow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouplowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
